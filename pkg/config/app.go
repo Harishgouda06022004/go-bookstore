c6eb77c37f5285c890351a75d4f6b7e39e01f769
Harishgouda G Patil <harishgouda213@gmail.com>
2025-04-26 10:39:47 +0000

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -8,7 +8,7 @@ var(
 )
 func connect(){
 	// dsn := "root:your_password@tcp(127.0.0.1:3306)/mysql"
-	d,err:=form.Open("mysql","root:2004/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d,err:=gorm.Open("mysql","root:2004/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err!=nil{
 		panic(err)
 	}
